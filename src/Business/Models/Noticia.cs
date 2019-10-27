edb527ca3993579a3b299f1fc89fffa3fc8ec6ea
Westefns <westefns@outlook.com.br>
2019-10-26 23:13:11 -0300

    Primeiro commit

---
+++ src/Business/Models/Noticia.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Models
+{
+    public class Noticia : Entity
+    {
+        public string Titulo { get; set; }
+    }
+}
